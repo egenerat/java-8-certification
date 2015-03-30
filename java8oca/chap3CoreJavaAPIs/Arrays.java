9c05f5e4b6fbb0c6b5fd0b21da23e2fc3651c9b3
egenerat <emile.g@outlook.com>
2015-03-30 20:13:04 +0100

    arrays

---
+++ java8oca/chap3CoreJavaAPIs/Arrays.java
@@ -0,0 +1,9 @@
+package chap3CoreJavaAPIs;
+
+public class Arrays{
+	public static void main(String[] args){
+		// Valid declarations
+		int[] array = {1,2,3};
+		int[] array2 = new int[]{1,2,3};
+	}
+}
