34d8678d099b3f56594254234c315d648d1702a4
egenerat <emile.generat@outlook.com>
2015-04-01 18:36:10 +0100

    updates

--- java8oca/chap3CoreJavaAPIs/Arrays.java
+++
@@ -1,9 +0,0 @@
-package chap3CoreJavaAPIs;
-
-public class Arrays{
-	public static void main(String[] args){
-		// Valid declarations
-		int[] array = {1,2,3};
-		int[] array2 = new int[]{1,2,3};
-	}
-}
---
+++ java8oca/chap3CoreJavaAPIs/Strings.java
@@ -0,0 +1,10 @@
+package chap3CoreJavaAPIs;
+
+public class Strings{
+	public static void main(String[] args){
+
+		//String Buffer declarations
+		StringBuffer sb = new StringBuffer();
+		StringBuffer sb2 = new StringBuffer("Initial string");
+	}
+}
