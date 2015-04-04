aa93b53e5fe9826f5e38d4d4ef5f7cb0d62aaf98
egenerat <emile.generat@outlook.com>
2015-04-04 15:13:20 +0100

    wild import

---
+++ java8oca/chap1Blocks/WildImport.java
@@ -0,0 +1,18 @@
+package chap1Blocks;
+
+import java.util.*;
+import java.sql.*;
+
+public class WildImport{
+	public static void main(String... args){
+		ArrayList<String> stringList = new ArrayList<String>();
+		// Test import conflict
+		//Date truc;
+		for(int i = 0; i<args.length; i++){
+			System.out.println(args[i]);
+			stringList.add(args[i]);
+		}
+		// Test variable out of scope
+		//System.out.println(i);
+	}
+}
