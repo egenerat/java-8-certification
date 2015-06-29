4d8c1d940b109a7e358ac473ec2f4c50e4d218cd
egenerat <emile.generat@outlook.com>
2015-06-29 09:53:18 +0100

    reference parameter

---
+++ offTopicExam/UpdateObjectParameter.java
@@ -0,0 +1,16 @@
+package offTopicExam;
+
+import java.util.HashMap;
+
+public class UpdateObjectParameter{
+	public static void main(String... args){
+		HashMap<String, String> map = new HashMap<>();
+		System.out.println("Size before: "+map.size());
+		new UpdateObjectParameter().method(map);
+		System.out.println("Size after: "+map.size());
+	}
+
+	public void method(HashMap<String, String> param){
+		param.put("Key","value");
+	}
+}
