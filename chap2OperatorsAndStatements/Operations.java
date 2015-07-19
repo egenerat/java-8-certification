b8de0a08ad7ddacc9bc536ef6cf1c0e80a5bb105
egenerat <emile.generat@outlook.com>
2015-07-19 15:47:57 +0100

    boolean equal

--- chap2OperatorsAndStatements/Operations.java
+++ chap2OperatorsAndStatements/Operations.java
@@ -11,5 +11,32 @@ public class Operations{
 
 		a = (short)1_921_222;
 		System.out.println(a);
+
+		char b = 'b';
+		char c = 'c';
+		// Not valid with double qutoes
+		// char d = "d";
+
+		// Not valid: a char (same for byte and short) is promoted to int before used with a binary operator
+		//char result = b + c;
+
+		String b_string = "b";
+	
+		// Forbidden
+		//String c_string = 'c';
+		//System.out.println(b == b_string);
+		System.out.println(b_string.equals(b));
+
+		boolean bool = true;
+		
+		// Forbidden, there is no relation between 0 and false!
+		//bool = (bool!=true);
+
+		// Forbidden!!!
+		//x = void_method();
+		//Object an_object = void_method();
+
 	}
+
+	void void_method(){};
 }
