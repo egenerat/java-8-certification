6578dc64df69bfcd8bfcc278fdcc5419fa66d560
egenerat <emile.generat@outlook.com>
2015-09-21 09:47:41 +0100

    string update

--- chap3CoreJavaAPIs/Strings.java
+++ chap3CoreJavaAPIs/Strings.java
@@ -6,5 +6,17 @@ public class Strings{
 		//String Buffer declarations
 		StringBuffer sb = new StringBuffer();
 		StringBuffer sb2 = new StringBuffer("Initial string");
+
+		// does not compile
+		// String aString = 1;
+
+		// even with explicit cast
+		//String aString = (String)1;
+
+		String aString2 = "string";
+
+		// Possible to concatenate a string and a char
+		aString2 += 'c';
+
 	}
 }
