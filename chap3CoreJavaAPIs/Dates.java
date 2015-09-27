0139a288d49f5921c6e2b829d873525a5d900f6b
egenerat <emile.generat@outlook.com>
2015-09-27 15:38:16 +0100

    Create a date object from a string representing a date

--- chap3CoreJavaAPIs/Dates.java
+++ chap3CoreJavaAPIs/Dates.java
@@ -4,8 +4,15 @@ import java.util.Calendar;
 import java.time.Month;
 import java.time.*;
 
-class Dates{
-	public static void main(String... args){
+class Dates {
+
+	public void createDateFromString() {
+		Date date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").parse("2012-05-20T09:00:00.000Z");
+		String formattedDate = new SimpleDateFormat("yyyy-MM-dd'T'HHmmss").format(date);
+		System.out.println(formattedDate);
+	}
+
+	public static void main(String... args) {
 		// Be careful, old API Calendar starts at 0, new Month starts at 1
 		System.out.println(Calendar.APRIL);
 		System.out.println(Month.APRIL);
