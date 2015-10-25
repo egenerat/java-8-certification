af922d198019beccb3ca12ab1112fd589536cbe2
egenerat <emile.generat@outlook.com>
2015-10-25 12:24:35 +0000

    new files, recap exam

---
+++ chap2OperatorsAndStatements/Cast.java
@@ -0,0 +1,20 @@
+package chap2OperatorsAndStatements;
+
+class Cast{
+	public static void main(String... args){
+		// incompatible types: char cannot be converted to String
+		// String a = 'a';
+		// either add a char to a String or use a StringBuilder or String.valueOf('c');
+		String a = ""+'a';
+
+		int anInt = 1;
+		// ClassCastException
+		// String test = (String)anInt;
+		String test = Integer.valueOf(anInt).toString();
+
+		int[] array = new int[]{1, 2, 3};
+		Object obj = array;
+		// ClassCastException 
+		// String[] array2 = (String[]) obj;
+	}
+}
---
+++ chap5ClassDesign/InterfaceMultipleInheritance.java
@@ -0,0 +1,11 @@
+package chap5ClassDesign;
+
+interface I1{}
+interface I2{}
+
+// An interface can inherits (extends) multiple interfaces, but cannot implement them
+interface I3 extends I1, I2{}
+
+public class InterfaceMultipleInheritance{
+
+}
