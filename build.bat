b7eff612b14d397535d65aa1b0795e4cc90ddc5c
unknown <emile.generat@LOND25607.iis.amadeus.net>
2016-01-14 09:52:08 +0000

    windows build

---
+++ build.bat
@@ -0,0 +1,6 @@
+dir /s /B *.java > sources.txt
+
+:: @ to tell Javac it's a file list
+javac @sources.txt
+
+"C:\Program Files\Java\jdk1.8.0_65\bin\javac" chap1Blocks/*.java chap2OperatorsAndStatements/*.java chap3CoreJavaAPIs/*.java chap4MethodsAndEncapsulation/*.java chap5ClassDesign/*.java chap6Exception/*.java package03_22_full_revisions/*.java offTopicExam/*.java  offTopicExam/*.java
