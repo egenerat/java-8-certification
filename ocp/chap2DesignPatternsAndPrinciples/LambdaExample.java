1a4a1ccd7d285239c554f4e6c8b785c72f2f8408
egenerat <emile.generat@outlook.com>
2016-05-28 00:42:38 +0100

    example lambdas

---
+++ ocp/chap2DesignPatternsAndPrinciples/LambdaExample.java
@@ -0,0 +1,32 @@
+package ocp.chap2DesignPatternsAndPrinciples;
+
+public class LambdaExample {
+
+	@FunctionalInterface
+	public interface NoReturnType {
+		void test();
+	}
+
+	public static void noReturnType(NoReturnType l) {
+		l.test();
+	}
+
+	public static void main(String... args) {
+		// TODO
+		// Allowed
+		noReturnType(() -> System.out.println());
+
+		// Do not compile
+		// noReturnType( -> System.out.println());
+		// => If 0 parameter, parenthesis are mandatory
+
+		/* () mandatory if:
+				- 0 parameter
+				- Types are specified
+
+			{} braces mandatory if:
+				- return ;
+				- more than one line
+		*/
+	}
+}
