fdcb95b4de2781ae2d99aa303560963b27d184c1
egenerat <emile.generat@outlook.com>
2016-06-02 22:11:20 +0100

    supplier example

---
+++ ocp/chap4FunctionalInterfaces/SupplierExample.java
@@ -0,0 +1,18 @@
+package ocp.chap4FunctionalInterfaces;
+
+import java.util.ArrayList;
+import java.util.function.Supplier;
+
+public class SupplierExample {
+	public static void main(String... args) {
+		Supplier<String> s1 = String::new;
+		Supplier<String> s2 = () -> new String();
+		System.out.println(s1.get().length());
+		System.out.println(s2.get().length());
+
+		Supplier<ArrayList<String>> s3 = ArrayList<String>::new;
+		ArrayList<String> list = s3.get();
+		System.out.println(list);
+	}
+}
+
