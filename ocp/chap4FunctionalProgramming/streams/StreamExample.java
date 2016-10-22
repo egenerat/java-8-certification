0e9d193a62cb4c5b9c0a42b4e2498b14536b9242
egenerat <emile.generat@outlook.com>
2016-10-22 22:42:08 +0100

    streams

---
+++ ocp/chap4FunctionalProgramming/streams/StreamExample.java
@@ -0,0 +1,37 @@
+package ocp.chap4FunctionalProgramming.streams;
+
+import java.util.stream.Stream;
+import java.util.List;
+import java.util.ArrayList;
+
+public class StreamExample {
+	public static void main(String... args) {
+		// ===========
+		// Terminal operations
+
+		// - Reduction
+		// identity set to 0. If identity is not set, the reduction returns an optional
+		Stream<Integer> stream = Stream.of(1, 2, 3);
+		Integer resultAddition = stream.reduce(0, (a, b) -> a+b);
+		System.out.println(resultAddition);
+
+		// ===========
+		// Intermediate operations
+
+		// Filter
+		stream = Stream.of(1, 2, 3);
+		Stream<Integer> filteredStream = stream.filter(a -> a > 1);
+		filteredStream.forEach(System.out::print); // 23
+
+		//==========
+		System.out.println("\nmap");
+		List<String> words = new ArrayList<>();
+		List<String> cities = new ArrayList<>();
+		words.add("Hello");
+		words.add("World");
+		cities.add("London");
+		Stream<List<?>> stream2 = Stream.of(words, cities);
+		//stream2.forEach(System.out::print);
+		stream2.peek(System.out::print).map(List::size).forEach(System.out::print);
+	}
+}
