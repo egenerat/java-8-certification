85f5a6bf92b5889e8f5d2abb6821512a3545018e
egenerat <emile.generat@outlook.com>
2016-10-23 22:21:29 +0100

    stream iterate

--- ocp/chap4FunctionalProgramming/streams/StreamExample.java
+++ ocp/chap4FunctionalProgramming/streams/StreamExample.java
@@ -33,5 +33,14 @@ public class StreamExample {
 		Stream<List<?>> stream2 = Stream.of(words, cities);
 		//stream2.forEach(System.out::print);
 		stream2.peek(System.out::print).map(List::size).forEach(System.out::print);
+
+
+		// Iterate
+		System.out.println("Iterate");
+		// First parameter is the seed
+		Stream<Integer> suite = Stream.iterate(2, n -> n + 1);
+
+		suite.limit(5).forEach(System.out::print);
+// Returns an infinite sequential ordered Stream produced by iterative application of a function f to an initial element seed, producing a Stream consisting of seed, f(seed), f(f(seed)), etc.
 	}
 }
